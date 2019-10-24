83dfd0d7a23054af261b805e8aaca6f01ebd3959
matejslovak <joggurtik1@gmail.com>
2019-10-24 08:54:45 +0200

    nvm

---
+++ HelloObjects/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloObjects
+{
+    class Greeter
+    {
+        public void Greet(string name)
+        {
+            Console.WriteLine("Hello user {0}", name);
+        }
+    }
+}
---
+++ HelloObjects/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HelloObjects;
+
+namespace OnlineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeter greeter = new Greeter();
+            greeter.Greet("Karol");
+            greeter.Greet("Peter");
+            Console.ReadKey();
+
+        }
+    }
+}
